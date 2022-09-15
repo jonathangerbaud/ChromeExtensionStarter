14250503a38d1364ff03f067a7b833df9a5d0dd7
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-09-15 17:16:04 +0100

    NX + TailwindCSS

---
+++ ReactJS/NX/With_TailwindCSS/react-app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./apps/react-app/src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
